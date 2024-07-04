2134334f0c2a9108906b4404498d706751e71666
MUHAMMED SINAN D <sinanshamsudheen7@gmail.com>
2024-07-04 22:03:58 +0530

    prnt all wrds from phone digits

---
+++ 7-8.cpp
@@ -0,0 +1,24 @@
+// print all words from phone digits
+#include<iostream>
+using namespace std;
+string KeypadArray[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+void keypad(string s,string ans){
+    if(s.length()==0){
+        cout<<ans<<endl;
+        return;
+    }
+    char ch=s[0];
+    string code = KeypadArray[ch-'0'];
+    string ros = s.substr(1);
+
+    for(int i=0;i<code.length();i++){
+        keypad(ros,ans+code[i]);
+
+    }
+}
+int main()
+{
+    keypad("23","");
+ return 0;
+}
