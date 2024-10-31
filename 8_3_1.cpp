8498d42a821ed948aa3018595d537aba3f7fe486
sinanshamsudheen <carreersins@gmail.com>
2024-10-31 20:35:28 +0530

    normal sort

---
+++ 8_3_1.cpp
@@ -0,0 +1,31 @@
+// Sorting
+#include<iostream>
+using namespace std;
+int sorting(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[i]){  
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    sorting(arr,n);
+    for(int i=0;i<n;i++){ 
+    cout<<arr[i]<<" ";
+    }cout<<endl;
+ return 0;
+}
+
