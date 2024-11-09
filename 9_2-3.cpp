c9ef9f4ca2c37a123d81624d85351111af4d25a6
sinanshamsudheen <carreersins@gmail.com>
2024-11-09 23:59:20 +0530

    2D matrix

---
+++ 9_2-3.cpp
@@ -0,0 +1,35 @@
+// 2D matrix search
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int target; cin>>target;
+    int mat[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>mat[i][j];
+        }
+    }
+    bool found = false;
+    int r=0, c=m-1;
+    while( r < n and c>=0){
+        if(mat[r][c]==target){
+            found = true;
+        }
+        if(mat[r][c]>target){
+            c--;
+        }
+        else{
+            r++;
+        }
+        if(found){
+            cout<<"Element found";
+        }
+        else{
+            cout<<"Not found";
+        }
+    }
+ return 0;
+}
