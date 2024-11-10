16651b08475eac6900c08515b66efe79a3888c4a
sinanshamsudheen <carreersins@gmail.com>
2024-11-10 13:56:05 +0530

    stack, heap and dynamic memory allocation

---
+++ 12.cpp
@@ -0,0 +1,17 @@
+// stack, heap and dynamic memory allocation
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10; //stored in stack
+    int *p = new int(); //allocate memory in heap
+    *p = 10;
+    delete(p); //deallocate memory
+
+    p=new int[4];
+    delete[]p;
+    p=NULL;
+
+ return 0;
+}
