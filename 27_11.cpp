9358ebfc48948ee5544c0cdac52ea7327ca2760f
sinanshamsudheen <carreersins@gmail.com>
2024-12-13 19:18:36 +0530

    left view

---
+++ 27_11.cpp
@@ -0,0 +1,54 @@
+ //balanced height
+#include "bits/stdc++.h"
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void leftView(Node* root){
+    if(root==NULL){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        int n = q.size();
+        for(int i=1;i<=n;i++){
+            Node* temp= q.front();
+            q.pop();
+
+            if(i == 1){
+                cout<<temp->data<<" ";
+            }
+            if(temp->left != NULL){
+                q.push(temp->left);
+            }
+            if(temp->right != NULL){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main(){
+    Node* root = new Node(5);
+    root->left = new Node(3);
+    root->right= new Node(6);
+    root->left->left=new Node(2);
+    root->left->right=new Node(4);
+
+    leftView(root);
+
+    return 0;
+}
