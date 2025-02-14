0b25289cee54ddc5082388345ad53de999d5e1a3
sinanshamsudheen <carreersins@gmail.com>
2025-02-14 19:50:25 +0530

    dp

---
+++ 35_14.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#include<vector>
+#include<climits>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<stack>
+#include<algorithm>
+#include<string>
+
+#define vi vector<int>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first 
+#define ss second
+#define setBits(x) builtin_popcount(x)
+using namespace std;
+const int N=1e3+2,MOD=1e9+7;
+
+int main()
+{
+    int n;cin>>n;
+    vi fib(n+2,0);
+    fib[0]=1;
+    fib[1]=1;
+    for(int i=2;i<=N+1;i++){
+        fib[i]=fib[n-1] + fib[n-2];
+    }
+    cout<<fib[n+1];
+ return 0;
+}
