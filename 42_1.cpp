4f5ade4daa8accbeecb13d1160f75c4c6a673864
sinanshamsudheen <carreersins@gmail.com>
2025-04-27 15:27:20 +0530

    dp

---
+++ 42_1.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+#define vi vector<int>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first 
+#define ss second
+#define setBits(x) builtin_popcount(x)
+using namespace std;
+const int N=1e3+2,MOD=1e9+7;
+
+int main()
+{
+    int n;cin>>n;
+
+    vi a(n);
+    rep(i,0,n){
+        cin>>a[i];
+    }
+    int len = sqrtl(n) + 1;
+    vi b(len);
+    for(int i=0;i<n;i++){
+        b[i/len] += a[i];
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        l--;r--;
+        int sum=0;
+        for(int i=l;i<=r;){
+            if(i%len == 0 && i+len-1 <= r){
+                sum += b[i/len];
+                i += len;
+            }
+            else{
+                sum += a[i];
+                i++;
+            }
+        }
+        cout<<sum<<endl;
+    }
+ return 0;
+
+}
